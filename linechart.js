1865daf90c26e40f2da367a56272b0a2935fc169
blarryy <34322953+blarryy@users.noreply.github.com>
2018-03-21 15:52:32 -0700

    Update linechart.js
    svg changes

--- linechart.js
+++ linechart.js
@@ -1,7 +1,7 @@
 
 
 var svg = d3.select("svg"),
-    margin = {top: 20, right: 520, bottom: 180, left: 50},
+    margin = {top: 20, right: 520, bottom: 230, left: 50},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
